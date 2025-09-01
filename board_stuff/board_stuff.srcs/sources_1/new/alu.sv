d3d76279c1e2f15135e0306aff30c7ec7f8072d1
bsgursta <bryansgonzalez156@gmail.com>
2025-09-01 10:25:25 -0400

    memory and ALU

---
+++ board_stuff/board_stuff.srcs/sources_1/new/alu.sv
@@ -0,0 +1,35 @@
+module ALU #
+(
+    parameter DATA_WIDTH = 8
+)(
+    input logic [DATA_WIDTH-1:0] inA,
+    input logic [DATA_WIDTH-1:0] inB
+    input logic [2:0] OPCode
+    output logic[DATA_WIDTH-1:0] outData
+    output logic[DATA_WIDTH-1:0] hiData
+);
+
+logic [DATA_WIDTH*2-1:0] product;
+
+
+
+always_comb begin
+    product = inA * inB;
+    hiData = '0;
+    
+
+    unique case(OPCode)
+        3'b000 : outData = inA + inB;
+        3'b001 : outData = inA - inB;
+        3'b010 : begin
+            outData = product[DATA_WIDTH*2-1:DATA_WIDTH];
+            hiData =  product[DATA_WIDTH*2-1:DATA_WIDTH]; 
+        end
+        3'b011 : outData = inA & inB;
+        3'b100 : outData = inA | inB;
+        default : outData = '0;
+    endcase;
+end
+
+endmodule
+
